975f05ebd54fb95e8c120ab4e4e359cfba6b1282
Lucas McDonald <lucasmcdonald3@gmail.com>
2017-04-05 11:52:53 -0500

    Create LEDController.py

---
+++ LEDController.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+import Pyro4
+
+def LEDController(self):
+
+    def __init__(self):
+
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(3, GPIO.OUT)
+        self.dutyCycle = 0
+        self.p = GPIO.PWM(3, self.dutyCycle)
+        self.p.start(self.dutyCycle)
+
+    def max(self):
+        self.dutyCycle = 0
+        self.p.ChangeDutyCycle(self.dutyCycle)
+
+    def min(self):
+        self.dutyCycle = 0
+        self.p.ChangeDutyCycle(self.dutyCycle)
+
+    def stepUp(self):
+        self.dutyCycle += 10
+        self.p.ChangeDutyCycle(self.dutyCycle)
+
+    def stepDown(self):
+        self.dutyCycle -= 10
+        self.p.ChangeDutyCycle(self.dutyCycle)
