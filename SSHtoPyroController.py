41a9e57dd43e9adfa23e31ae5501eb12d3fcde1e
Lucas McDonald <lucasmcdonald3@gmail.com>
2017-05-15 15:33:24 -0500

    Create SSHtoPyroController.py

---
+++ SSHtoPyroController.py
@@ -0,0 +1,28 @@
+# This file converts the SSH command sent to the device into usable data for the Raspberry Pi.
+# The name cannot be changed as it is explicitly defined in the HomeRemote app.
+# Also, the "if(arg == "max")" statements have explicitly defined test strings (max, min, stepUp, stepDown).
+# These are explicitly defined in the HomeRemote app and should not be modified.
+# However, anything else can be modified. HomeRemote simply provides a platform to execute your own code from a remote.
+
+import sys
+import time
+import LEDController
+
+# LEDController uses the Stepper Remote, which takes in one argument.
+arg = sys.argv[1]
+
+# Create the LEDController object that will manage the LED.
+ledObject = LEDController.LEDController()
+
+# Based on the argument in the command, tell the LEDController to do something.
+# You CANNOT change the equality tests in the if statement.
+# The test strings are explicitly defined in the HomeRemote app and cannot be modified.
+if(arg == "max"):
+    ledObject.max()
+elif(arg == "min"):
+    proxymotor.min()
+elif(arg == "stepUp"):
+    print(proxymotor.inchesMotorMover(float(arg1), float(arg2)))
+elif(arg == "stepDown"):
+    print(proxymotor.radecScan(float(arg1), float(arg2)))
+exit()
